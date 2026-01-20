c3c3dcafc11c9cc3284af9b1331666ef4b9cc38f
Nicholas-Dunstan <nicholas@wuwei.digital>
2026-01-20 16:10:49 +1000

    Update index.ts

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -1,4 +1,3 @@
 export { default as Header } from "./Header";
 export { default as Sidebar } from "./Sidebar";
 export { default as MobileNav } from "./MobileNav";
-export { default as ThemeToggle } from "./ThemeToggle";
